a48a2fa8442e1207837d76f87af4e3da74670d63
RamildoJunior <rpsjuninho@gmail.com>
2025-01-19 21:35:37 -0300

    fechando a parte pratica do dia

---
+++ src/main/java/com/mold/springboot3/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.mold.springboot3.repositories;
+
+import com.mold.springboot3.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
